da12ea48ecc9428531f8ed7596d3f6d621af01c6
Taim-Amer <taim.amer.000@gmail.com>
2025-01-25 04:33:26 -0800

    Drivers map

--- lib/features/orders/views/order/drivers_map.dart
+++ lib/features/orders/views/order/drivers_map.dart
@@ -1,17 +1,22 @@
 import 'package:comprehensive_pharmacy_pharmacy_role/common/widgets/alerts/snackbar.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/common/widgets/appbar/appbar.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/common/widgets/buttons/back_icon.dart';
+import 'package:comprehensive_pharmacy_pharmacy_role/common/widgets/map/current_marker.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/features/orders/models/driver_model.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/features/orders/views/order/widgets/driver_details_bottom_sheet.dart';
+import 'package:comprehensive_pharmacy_pharmacy_role/utils/constants/api_constants.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/utils/constants/colors.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/utils/constants/enums.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/utils/constants/image_strings.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/utils/constants/text_strings.dart';
+import 'package:comprehensive_pharmacy_pharmacy_role/utils/helpers/helper_functions.dart';
 import 'package:comprehensive_pharmacy_pharmacy_role/utils/router/app_router.dart';
+import 'package:comprehensive_pharmacy_pharmacy_role/utils/services/map_services.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:latlong2/latlong.dart';
 
 class DriversMap extends StatefulWidget {
   final List<Drivers> drivers;
@@ -26,36 +31,27 @@ class DriversMap extends StatefulWidget {
 }
 
 class _UserLocationMapState extends State<DriversMap> {
-  late MapController controller;
+  final MapController _mapController = MapController();
 
   @override
   void initState() {
     super.initState();
 
-    final initialPosition = widget.drivers.isNotEmpty ? GeoPoint(
-      latitude: double.parse(widget.drivers.first.lat!),
-      longitude: double.parse(widget.drivers.first.lng!),
-    ) : GeoPoint(latitude: 0, longitude: 0);
-
-    controller = MapController(
-      initPosition: initialPosition,
-      areaLimit: const BoundingBox(
-        east: 100,
-        north: 86.0,
-        south: -86.0,
-        west: -100,
-      ),
-    );
+    final initialPosition = widget.drivers.isNotEmpty ? LatLng(
+      double.parse(widget.drivers.first.lat!),
+      double.parse(widget.drivers.first.lng!),
+    ) : const LatLng(0, 0);
 
     WidgetsBinding.instance.addPersistentFrameCallback((_) async {
       if (widget.drivers.isNotEmpty) {
-        await controller.goToLocation(initialPosition);
+        _mapController.move(initialPosition, 15);
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: TAppBar(actions: [
         TextButton(
@@ -66,38 +62,42 @@ class _UserLocationMapState extends State<DriversMap> {
           child: Text(TEnglishTexts.next, style: Theme.of(context).textTheme.labelLarge?.copyWith(fontWeight: FontWeight.bold, color: TColors.primary)),
         )],),
       // floatingActionButton: FloatingActionButton(onPressed: () => setState(() {})),
-      body: OSMFlutter(
-        controller: controller,
-        onGeoPointClicked: (geoPoint) {
-          final driver = widget.drivers.firstWhere((driver) => double.parse(driver.lat!) == geoPoint.latitude && double.parse(driver.lng!) == geoPoint.longitude);
-          showDriverDetailsBottomsheet(name: driver.name ?? '', phone: driver.phone ?? '', distance: driver.distance ?? 0, driverID: driver.id!);
-          },
-        osmOption: OSMOption(
-          userTrackingOption: const UserTrackingOption(
-            enableTracking: false,
-            unFollowUser: false,
+      body: Column(
+        children: [
+          Expanded(
+            child: ValueListenableBuilder<LatLng?>(
+              valueListenable: TMapServices.currentLocationNotifier,
+              builder: (context, currentLocation, child) {
+                return FlutterMap(
+                  mapController: _mapController,
+                  options: MapOptions(
+                    initialCenter: currentLocation ?? const LatLng(0, 0),
+                    initialZoom: 15,
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate: dark ? TApiConstants.darkMap : TApiConstants.lightMap,
+                    ),
+                    MarkerLayer(markers: [
+                      ...widget.drivers.map((driver) => Marker(
+                        point: LatLng(double.parse(driver.lat!), double.parse(driver.lng!)),
+                        child: GestureDetector(
+                            onTap: () {
+                              final driver = widget.drivers.firstWhere((driver) => double.parse(driver.lat!) == LatLng(double.parse(driver.lat!), double.parse(driver.lng!)).latitude &&
+                                    double.parse(driver.lng!) == LatLng(double.parse(driver.lat!), double.parse(driver.lng!)).longitude,
+                              );
+                              showDriverDetailsBottomsheet(name: driver.name ?? '', phone: driver.phone ?? '', distance: driver.distance ?? 0, driverID: driver.id!);
+                            },
+                            child: SvgPicture.asset(TImages.motorIcon)),
+                        ),
+                      ),
+                    ])
+                  ],
+                );
+              },
+            ),
           ),
-          zoomOption: const ZoomOption(
-            initZoom: 15,
-            minZoomLevel: 3,
-            maxZoomLevel: 19,
-            stepZoom: 1.0,
-          ),
-          staticPoints: [
-            ...widget.drivers.map((driver) => StaticPositionGeoPoint(
-              driver.id.toString(),
-              MarkerIcon(
-                iconWidget: SvgPicture.asset(TImages.motorIcon),
-              ),
-                [
-                  GeoPoint(
-                    latitude: double.parse(driver.lat!),
-                    longitude: double.parse(driver.lng!),
-                  )
-                ],
-            ))
-          ],
-        ),
+        ],
       ),
     );
   }
